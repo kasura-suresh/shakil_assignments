335a62a7465a7397d465a2e5b034db3a93776240
Shakil-ansari92 <shakilansari9985@gmail.com>
2020-03-13 03:18:56 -0700

    C++ Assignment

--- 31ostream.cpp
+++ 31ostream.cpp
@@ -1,22 +1,52 @@
-/*************************************************************
-    C++ programe to implement Ostream operator 
-    author : MD SHAKIL ANSARI
- *************************************************************/
+/**************************************************** 
+    C++ program implement OSTREAM operator overloading 
+    author : MD SHAKIL ANSARI ,date - 12-mar,2020
+ ****************************************************/
 
 
-#include <iostream> 
+#include<iostream> 
 using namespace std; 
-  
+
+class Complex 
+{ 
+private: 
+	int real, imag; 
+public:
+    Complex() // for fixing error
+	{
+	 cout<<"one object is created "<<endl ;	
+	} 
+	Complex(int r , int i ) 
+	{ 
+	real = r; 
+	imag = i; 
+	} 
+	friend void operator << (ostream &output, Complex &c); // frnd fun to access private member
+	friend void operator >> (istream &input, Complex &c); 
+}; 
+void operator << (ostream& output, Complex& c) // output and c1 is copied here as parameter
+{ 
+    output<<"The complex number is "<<endl ;
+	output << c.real; 
+	output <<" + "<<c.imag <<"i" <<endl; 
+	
+} 
+
+void operator >> (istream& input, Complex& c) // input and c1 is copied here as parameter
+{ 
+    
+        cout << "Enter Real Part "; 
+	input >> c.real; 
+	cout << "Enter Imaginary Part "; 
+	input >> c.imag; 
+	 
+} 
+
 int main() 
 { 
-    char x;
-    cout<<"enter a character\n" ;
-    cin.get(x); // used to scan a single character ,an istream file 
-    cout<<"this is what you entered\n" ;
-    cout.put(x);  // ostream file to print single char 
-    cout<<endl;
-    cout.write("Ostream example",15); // public method to write a string
-    cout<<"\n";
-    cout.write("Ostream example",7);
-    return 0;
-} 
+Complex c1; 
+cin >> c1; // calling overloaded oprtr " >> " 
+cout << c1; // calling overloaded oprtr " << " 
+return 0; 
+}
+
